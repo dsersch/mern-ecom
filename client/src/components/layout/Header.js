b1592dbaf3cd7834f466e62cc1ca19d0f424387c
dave <dsersch640@gmail.com>
2022-02-16 15:41:06 -0500

    icons and nav bar start

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -2,9 +2,14 @@ import classes from './Header.module.css'
 
 const Header = () => {
     return (
-        <div className={classes['header']}>
-            header
-        </div>
+        <nav className={classes['nav-bar']}>
+            <p><i className='fa-solid fa-gamepad'></i>GameStore</p>
+            <ul className={classes['nav-link-container']}>
+                <li><a href='#' className={classes['nav-link']}><i className='fa-solid fa-house'></i>Home</a></li>
+                <li><a href='#' className={classes['nav-link']}><i className='fa-solid fa-cart-shopping'></i>Cart</a></li>
+                <li><a href='#' className={classes['nav-link']}><i className='fa-solid fa-user'></i>Login</a></li>
+            </ul>
+        </nav>
     )
 }
 
