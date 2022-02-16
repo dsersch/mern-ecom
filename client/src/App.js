4a88498fd2763c171affbb4fc9e4b58af0313ba5
dave <dsersch640@gmail.com>
2022-02-16 14:34:13 -0500

    css modules guesswork

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
 
 const App = () => {
   return (
     <div className="App">
-      <h2>working</h2>
+      <Header />
+      <main>
+        <h2>working</h2>
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import classes from "./Footer.module.css"
+
+const Footer = () => {
+    return (
+        <div className={classes['footer']}>
+            footer
+        </div>
+    )
+}
+
+export default Footer
