adbc75b44cf53c39d19adceacb11f3b6cc67e3b3
dave <dsersch640@gmail.com>
2022-02-22 16:07:40 -0500

    card started

--- client/src/components/screens/Home.jsx
+++ client/src/components/screens/Home.jsx
@@ -5,6 +5,8 @@ const Home = () => {
     return (
         <main className={classes['home']}>
             <Card />
+            <Card />
+            <Card />
         </main>
     )
 }
--- client/src/components/utility/Card.jsx
+++ client/src/components/utility/Card.jsx
@@ -3,7 +3,12 @@ import classes from './Card.module.css'
 const Card = () => {
     return (
         <div className={classes['card']}>
-            <h1>Working...</h1>
+            <div className={classes['image-area']}>
+            </div>
+            <div className={classes['info-box']}>
+                <p className={classes['description']}>this is test description text. this is test description text.</p>
+                <button className={classes['add-to-cart']}>$20.99</button>
+            </div>
         </div>
     )
 }
