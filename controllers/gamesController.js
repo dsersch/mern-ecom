3b424a8fae2153cd03ff65892a6a037040ee5665
dave <dsersch640@gmail.com>
2022-02-24 17:25:00 -0500

    unsaved file

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,16 +1,14 @@
 const axios = require('axios').default
 
-module.exports = {
-    getRecent: async (req, res) => {
-        const date = new Date()
-        let prevDate = new Date()
-        prevDate.setDate(prevDate.getDate() - 30)
-        const currentDateString = date.toISOString().substring(0, 10)
-        const previousDateString = prevDate.toISOString().substring(0, 10)
-        const url = `https://api.rawg.io/api/games?key=${process.env.RAWGKEY}&dates=${previousDateString},${currentDateString}&order=added&page_size=9&page=1`
+exports.getRecent = async (req, res) => {
+    const date = new Date()
+    let prevDate = new Date()
+    prevDate.setDate(prevDate.getDate() - 30)
+    const currentDateString = date.toISOString().substring(0, 10)
+    const previousDateString = prevDate.toISOString().substring(0, 10)
+    const url = `https://api.rawg.io/api/games?key=${process.env.RAWGKEY}&dates=${previousDateString},${currentDateString}&order=added&page_size=9&page=1`
     
-        const response = await axios.get(url)
+    const response = await axios.get(url)
         
-        res.json(response.data.results)
-    },
+    res.json(response.data.results)
 }
