e598203f77e19251621e526aec55a917ed383ccc
dave <dsersch640@gmail.com>
2022-03-01 15:57:11 -0500

    header cart notification

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,7 +1,11 @@
 import classes from './Header.module.css'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+    const cart = useSelector(state => state.cart)
+    const cartLenght = cart.cartItems.length
+
     return (
         <nav className={classes['nav-bar']}>
             <p>GameStore<i className='fa-solid fa-gamepad'></i></p>
@@ -12,10 +16,16 @@ const Header = () => {
                     </li>
                 </Link>
                 <Link to='/cart'>
-                    <li><button className={classes['nav-link']}><i className='fa-solid fa-cart-shopping'></i>Cart</button></li>
+                    <li>
+                        <button className={classes['nav-link']}><i className='fa-solid fa-cart-shopping'></i>Cart</button>
+                        
+                    </li>
                 </Link>
+                {cartLenght > 0 && <div className={classes['cart-count']}>{cartLenght}</div>}
                 <Link to='/login'>
-                    <li><button className={classes['nav-link']}><i className='fa-solid fa-user'></i>Login</button></li>
+                    <li>
+                        <button className={classes['nav-link']}><i className='fa-solid fa-user'></i>Login</button>
+                    </li>
                 </Link>
             </ul>
         </nav>
